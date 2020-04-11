28c6c23490f828222624cdc0fd432fa05ccfd87b
Artjoms <artjoms.mohorins@gmail.com>
2020-04-11 09:06:02 +0300

    04.11.2020

---
+++ src/main/java/controller/Menu.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class Menu {
+
+    public void mainHeader(){
+        System.out.println("+---------------------------------------+");
+        System.out.println("! Welcome to menu! ");
+        System.out.println("! Menu application !");
+        System.out.println("+---------------------------------------+");
+    }
+
+    public void printMenu(){
+        System.out.println("Please make a selection");
+        System.out.println("1) Tell me a color");
+        System.out.println("2) Tell me a shape");
+        System.out.println("0) Exit");
+    }
+
+}
